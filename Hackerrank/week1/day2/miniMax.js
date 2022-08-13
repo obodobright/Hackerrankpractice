1edd6f72e8faa19b61c545359ca317ea5237e089
Bright-Lumen <72477636+obodobright@users.noreply.github.com>
2022-08-13 23:05:18 +0100

    Update miniMax.js

--- Hackerrank/week1/day2/miniMax.js
+++ Hackerrank/week1/day2/miniMax.js
@@ -1,4 +1,4 @@
-// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers
 
 function miniMaxSum(arr) {
   // to get the maximum and minimu sum value of the arr.
